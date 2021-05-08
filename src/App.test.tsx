86679bf97f97af702ef1366774da4a759f170a21
SanthoshKumar9028 <71515865+SanthoshKumar9028@users.noreply.github.com>
2021-05-08 23:09:02 +0530

    changed all pages as lazy load

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import { screen } from "@testing-library/react";
 import App from "./App";
 import { renderWithRedux } from "./utils/render-with-redux";
 
-test("renders learn react link", () => {
+test("renders learn react link", async () => {
   renderWithRedux(<App />);
-  const linkElement = screen.getByText(/quiz app/i);
+  const linkElement = await screen.findByText(/quiz app/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,41 @@
-import { memo } from "react";
+import { memo, lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import "./App.scss";
-import Home from "./pages/home";
-import TestQuizs from "./pages/test-quizs";
-import PageNotFound from "./pages/404";
+
+const Home = lazy(() => import("./pages/home"));
+const TestQuizs = lazy(() => import("./pages/test-quizs"));
+const PageNotFound = lazy(() => import("./pages/404"));
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/html">
-          <TestQuizs language="html" />
-        </Route>
-        <Route path="/css">
-          <TestQuizs language="css" />
-        </Route>
-        <Route path="/javascript">
-          <TestQuizs language="javascript" />
-        </Route>
-        <Route path="/typescript">
-          <TestQuizs language="typescript" />
-        </Route>
-        <Route path="/python">
-          <TestQuizs language="python" />
-        </Route>
-        <Route path="/java">
-          <TestQuizs language="java" />
-        </Route>
-        <Route>
-          <PageNotFound />
-        </Route>
-      </Switch>
+      <Suspense fallback={<p>loading...</p>}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/html">
+            <TestQuizs language="html" />
+          </Route>
+          <Route path="/css">
+            <TestQuizs language="css" />
+          </Route>
+          <Route path="/javascript">
+            <TestQuizs language="javascript" />
+          </Route>
+          <Route path="/typescript">
+            <TestQuizs language="typescript" />
+          </Route>
+          <Route path="/python">
+            <TestQuizs language="python" />
+          </Route>
+          <Route path="/java">
+            <TestQuizs language="java" />
+          </Route>
+          <Route>
+            <PageNotFound />
+          </Route>
+        </Switch>
+      </Suspense>
     </Router>
   );
 }
